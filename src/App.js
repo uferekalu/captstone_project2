cf2b323e39b89f55a3b3409f49eeda6411900e58
jjatuah <93829162+jjatuah@users.noreply.github.com>
2021-11-30 02:16:43 +0100

    Update App.js
    worked on the loading message functionality

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ const url = "https://course-api.com/react-tours-project";
 
 function App() {
   const [tours, setTours] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
     fetch(url).then(response => response.json()).then(data => setTours(data));
     setLoading(false);
   }, []);
 
   return (
     <div className="main">
-      <h1 className="title">Our Tours</h1>
+      {!loading && <h1 className="title">Our Tours</h1>}
       <p className="underline"></p>
       {loading
         ? <Loading />
         : tours?.map(tour =>
             <Tours
               key={tour.id}
+
               name={tour.name}
               info={tour.info}
               image={tour.image}
