b0e3566c5ab3ce13da81eca4d8dfd74abe9de7a1
Williams Samuel <samwill300@gmail.com>
2021-11-30 03:15:38 +0100

    implemented the show more/show less logic and restyled the page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Loading from "./components/Loading";
-import Tours from "./components/Tours";
-import "./App.css";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Loading from './components/Loading'
+import Tours from './components/Tours'
+import './App.css'
 
-const url = "https://course-api.com/react-tours-project";
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  const [tours, setTours] = useState([]);
-  const [loading, setLoading] = useState(false);
+	const [tours, setTours] = useState([])
+	const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    setLoading(true);
-    fetch(url).then(response => response.json()).then(data => setTours(data));
-    setLoading(false);
-  }, []);
+	useEffect(() => {
+		setLoading(true)
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => setTours(data))
+		setLoading(false)
+	}, [])
 
-  return (
-    <div className="main">
-      <h1 className="title">Our Tours</h1>
-      <p className="underline"></p>
-      {loading
-        ? <Loading />
-        : tours?.map(tour =>
-            <Tours
-              key={tour.id}
-              name={tour.name}
-              info={tour.info}
-              image={tour.image}
-              price={tour.price}
-            />
-          )}
-    </div>
-  );
+	return (
+		<main className='main'>
+			<h1 className='title'>Our Tours</h1>
+			<p className='underline'></p>
+
+			<section className='tours'>
+				{loading ? (
+					<Loading />
+				) : (
+					tours?.map((tour) => (
+						<Tours
+							key={tour.id}
+							name={tour.name}
+							info={tour.info}
+							image={tour.image}
+							price={tour.price}
+						/>
+					))
+				)}
+			</section>
+		</main>
+	)
 }
 
-export default App;
+export default App
