6b9a328c64b1fcc87fdc8c9225943f7ba4ee9ed1
Ufere Kalu <dekalusha@gmail.com>
2021-12-01 01:06:08 -0600

    added loading

--- src/App.js
+++ src/App.js
@@ -8,38 +8,43 @@ const url = 'https://course-api.com/react-tours-project'
 
 function App() {
 	const [tours, setTours] = useState([])
-	const [loading, setLoading] = useState(false)
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
-		setLoading(true)
-		fetch(url)
-			.then((response) => response.json())
-			.then((data) => setTours(data))
-		setLoading(false)
-	}, [])
+		const fetchData = async () => {
+			setLoading(true);
+			const res = await fetch(url);
+			const response = await res.json();
+			setTours(response);
+			setLoading(false)
+		};
+		fetchData();
+	}, [setTours]);
 
 	return (
 		<main className='main'>
-			<h1 className='title'>Our Tours</h1>
-			<p className='underline'></p>
-
-			<section className='tours'>
-				{loading ? (
-					<Loading />
-				) : (
-					tours?.map((tour) => (
-						<Tours
-							key={tour.id}
-							name={tour.name}
-							info={tour.info}
-							image={tour.image}
-							price={tour.price}
-						/>
-					))
-				)}
-			</section>
+			{loading ? (
+				<Loading />
+			) : (
+				<>
+					<h1 className='title'>Our Tours</h1>
+					<p className='underline'></p>
+						<section className='tours'>
+						{
+							tours?.map((tour) => (
+								<Tours
+									key={tour.id}
+									name={tour.name}
+									info={tour.info}
+									image={tour.image}
+									price={tour.price}
+								/>
+							))
+						}
+					</section>
+				</>
+			)}
 		</main>
 	)
 }
-
 export default App
